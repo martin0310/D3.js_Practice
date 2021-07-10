14859ad736fde2c566f1a58fa021c8f5277ebd41
martin <tw0983978176@gmail.com>
2021-07-10 21:07:26 +0800

    add line

--- Population Density/index.js
+++ Population Density/index.js
@@ -1,4 +1,4 @@
-import {select, csv,scaleLinear,max,scaleBand,axisLeft,axisBottom} from 'd3';
+import {select, csv,scaleLinear,max,scaleBand,axisLeft,axisBottom,format} from 'd3';
 
 const svg = select('svg');
 
@@ -10,7 +10,7 @@ const render = data => {
   
   const xValue = d => d.populationDensity;
   const yValue = d => d.country;
-  const margin = {top : 20, right : 20,left : 130, bottom : 20};
+  const margin = {top : 40, right : 20,left : 130, bottom : 50};
   const innerWidth = width - margin.left - margin.right;
   const innerHeight = height - margin.top - margin.bottom;
   
@@ -24,23 +24,54 @@ const render = data => {
   	.padding(0.1);
   
   const yAxis = axisLeft(yScale);
-  const xAxis = axisBottom(xScale);
+  
+  /*const xAxisTickFormat = number => 
+  	format('.3s')(number)
+  		.replace('G','B');*/
+  
+  const xAxis = axisBottom(xScale)
+  	.tickSize(-innerHeight);
   
 	svg.append('text')
   	.text('Population Density Unit(People/1000 x km²)')
-  	.attr('transform',`translate(20,${height/2 + 200}) rotate(-90)`)
+  	.attr('transform',`translate(20,${height/2 + 200}) rotate(-90)`);
   	
-  	
+  
   
   const g = svg.append('g')
   	.attr('transform',`translate(${margin.left},${margin.top})`);
   
+  const text = svg.append('text')
+  	.text('Top 20 Densest Cities or Regioins')
+  	.attr('transform','translate(250,30)')
+  	.attr('class','title');
   
+  text.style("font-size", "30px");
+
+  /*const text = svg.append('text')
+  	.text('Top 10 Densest City Or Regioin');*/
   	
+  /*text.style("font-size", "25px");	*/
   
-  g.append('g').call(yAxis);
-  g.append('g').call(xAxis)
+  g.append('g').call(yAxis)
+  	.selectAll('.domain, .tick line')
+  	.remove();
+  
+  const xAxisG = g.append('g').call(xAxis)
   	.attr('transform',`translate(0,${innerHeight})`);
+    
+  
+  xAxisG.select('.domain')
+  	.remove();
+  
+
+  
+  xAxisG.append('text')
+  	.attr('y',40)
+  	.attr('x',innerWidth / 2)
+  	.attr('fill','black')
+  	.text('Density');
+  
   /*g.append('text')
 		.text("hello")
   	.attr('transform',`translate(0,${innerHeight + 35})`);*/
